2b0d371b5d4ff31c8a2fe051762d7fa811a16732
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-05-20 08:12:49 -0400

    time to sdlkfjfjlsd

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -1,3 +1,46 @@
 import React from 'react';
 
-export default () => <div>Post New</div>
+export default class PostNew extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      dietaryRestriction: '',
+      description: '',
+      isHomemade: false,
+      restaurant: '',
+    }
+  }
+  
+  handleTitle = (title) => {
+    this.setState({ title });
+  }
+  handleDiet = (dietaryRestriction) => {
+    this.setState({ dietaryRestriction });
+  }
+  handleDescription = (description) => {
+    this.setState({ description });
+  }
+
+  render(){
+    return (
+      <div>
+        <form method="post">
+          <input 
+            type="text" 
+            placeholder="Title" 
+            value={title}
+            onChange={e => this.handleTitle(e.target.value)}  
+          />
+          <input 
+            type="text" 
+            placeholder="diet" 
+            value={dietaryRestriction}
+            onChange={e => this.handleDiet(e.target.value)}
+          />              
+          <button type="submit" onClick={this.handleVerify}>Submit</button>
+        </form>   
+      </div>
+    )
+  }
+}
--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default () => <div>Post Show</div>
+export default class PostShow extends React.Component {
+  componentDidMount(){
+    
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Title</h2>
+        <h3>Homemade/Restaurant</h3>
+        <p>description</p>
+        <img src=""/>
+        <div>Tags</div>
+        <div>DroolPoints</div>
+        <div>Share</div>
+        <div>Map</div>
+        <div>Recipe</div>
+      </div>
+    )
+  }
+}
